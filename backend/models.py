6ad334d22b79239a270b0a0ef31211ee29178f45
Cody Bontecou <bontecouc@gmail.com>
2019-05-17 13:50:33 -0700

    Got app working with postgresql

--- backend/models.py
+++ backend/models.py
@@ -8,8 +8,8 @@ class Zone(models.Model):
     '''
     Object representing the zone that each mob resides in
     '''
-    id = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=255)
+    id = models.TextField(primary_key=True)
+    name = models.TextField(blank=True)
     image = models.TextField()
 
     def __str__(self):
@@ -24,20 +24,20 @@ class Mob(models.Model):
     '''
     Object representing each rare mob that resides within a zone.
     '''
-    name = models.CharField(max_length=255, blank=True)
+    name = models.TextField(blank=True)
     image = models.TextField(blank=True)
-    level = models.CharField(max_length=2)
-    health = models.CharField(max_length=255, blank=True)
-    damage = models.CharField(max_length=255, blank=True)
-    armor = models.CharField(max_length=255, blank=True)
-    model_id = models.CharField(max_length=255, blank=True)
-    faction = models.CharField(max_length=255, blank=True)
-    x_coordinate = models.CharField(max_length=255, blank=True)
-    y_coordinate = models.CharField(max_length=255, blank=True)
-    coordinates = models.CharField(max_length=255, blank=True)
+    level = models.TextField(blank=True)
+    health = models.TextField(blank=True)
+    damage = models.TextField(blank=True)
+    armor = models.TextField(blank=True)
+    model_id = models.TextField(blank=True)
+    faction = models.TextField(blank=True)
+    x_coordinate = models.TextField(blank=True)
+    y_coordinate = models.TextField(blank=True)
+    coordinates = models.TextField(blank=True)
     zone = models.ForeignKey('Zone', on_delete=models.CASCADE)
     items = models.ManyToManyField('Item', blank=True)
-    websites_id = models.CharField(max_length=10, blank=True)
+    websites_id = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -47,12 +47,12 @@ class Item(models.Model):
     '''
     Object representing the items that drop off of a mob
     '''
-    drop_rate = models.CharField(max_length=255, blank=True)
-    i_level = models.CharField(max_length=255, blank=True)
+    drop_rate = models.TextField(blank=True)
+    i_level = models.TextField(blank=True)
     image = models.TextField(blank=True)
-    name = models.CharField(max_length=255, blank=True)
-    required_level = models.CharField(max_length=255, blank=True)
-    type = models.CharField(max_length=255, blank=True)
+    name = models.TextField(blank=True)
+    required_level = models.TextField(blank=True)
+    type = models.TextField(blank=True)
 
     def __str__(self):
         return self.name

d6bb95bb05786eb1e1c18db4609d0daccb83f6f8
Cody Bontecou <bontecouc@gmail.com>
2019-08-26 09:13:59 -0600

    updated whitelist to include https://wowares.com

--- wow_rares/settings.py
+++ wow_rares/settings.py
@@ -132,11 +132,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
-# CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = (
-    'wowrares.com'
+    "https://wowrares.com"
 )
-# CORS_ALLOW_HEADERS = ('cache-control', 'Access-Control-Allow-Origin')
 
 
 import django_heroku
